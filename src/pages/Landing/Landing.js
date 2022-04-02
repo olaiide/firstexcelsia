0cdfff8ac2461e6f5ac31cd072e9c57c61192674
olaiide <horlaide909@gmail.com>
2022-04-02 00:00:26 -1200

    landing

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -6,10 +6,16 @@ import {
   HeroOne,
   HeroTwo,
   HeroThree,
+  WrapperThree,
 } from "./LandingElements";
 import HeroOneImage from "../../assets/HeroOne.svg";
+import HeroTwoImage from "../../assets/HeroTwo.svg";
+import HeroThreeImage from "../../assets/HeroThree.svg";
 import Playstore from "../../assets/Playstore.svg";
-import Appstore from "../../assets/Appstore.svg"
+import Appstore from "../../assets/Appstore.svg";
+import Whatsapp from "../../assets/Whatsapp.svg";
+import Mail from "../../assets/Mail.svg";
+import Button from "../../components/Button/Button";
 const Landing = () => {
   return (
     <Container>
@@ -23,23 +29,54 @@ const Landing = () => {
               <span>advance</span> humanity
             </h3>
             <p>
-              We would strive to achieve that through <br />providing education and
-              quality health
+              We would strive to achieve that through <br />
+              providing education and quality health
             </p>
-            <div>
-                <h3>DOWNLOAD APP</h3>
-                <div><img /></div>
+            <div className="apps">
+              <h4>DOWNLOAD APP</h4>
+              <div className="app__wrapper">
+                <img src={Playstore} alt='playstore' width="80px" height="30px" />
+                <img src={Appstore} alt='appstore' width="80px" height="30px" />
+              </div>
             </div>
           </div>
           <div className='second__content'>
             <div>
-              <img src={HeroOneImage} alt='hero one svg' style={{width : '100%'}} />
+              <img
+                src={HeroOneImage}
+                alt='hero one svg'
+                width="100%"
+              />
             </div>
           </div>
         </HeroOne>
       </WrapperOne>
-      <HeroTwo></HeroTwo>
-      <HeroThree></HeroThree>
+      <HeroTwo>
+          <div className="first__content">
+            <h3>Everything you’ll <br />need to <span>excel</span> in life</h3>
+            <p>We’ve curated a list of valuable resources<br />to get you going in life, all for free!</p>
+            <Button children={'Get Started'} />
+          </div>
+          <div className="second__content">
+            <div><img src={HeroTwoImage} alt="second" width="100%" height="100%"/></div>
+          </div>
+      </HeroTwo>
+      <WrapperThree>
+      <HeroThree>
+          <div className="first__content">
+              <h3>Contact us</h3>
+              <p>Got any questions, feedback, request and <br />
+              complains? Reach out...</p>
+              <div className="contact__">
+                  <div><img src={Whatsapp}alt="whatsapp icon" width="30px"/><p>+2348232323892</p></div>
+                  <div><img src={Mail} alt="mail icon" width="30px"/><p>contact@us.com</p></div>
+              </div>
+          </div>
+          <div className="second__content">
+              <div><img src={HeroThreeImage} alt="three" width="100%"/></div>
+          </div>
+      </HeroThree>
+      </WrapperThree>
     </Container>
   );
 };
--- src/pages/Landing/LandingElements.js
+++ src/pages/Landing/LandingElements.js
@@ -4,10 +4,12 @@ export const Container = styled.div`
   min-height: 100vh;
   width: 100%;
   margin: 0 auto;
+  font-family: Chalkboard SE;
 `;
 export const WrapperOne = styled.div`
   background-color: #301446;
   overflow: hidden;
+  height: 380px;
 `;
 
 export const HeroOne = styled.div`
@@ -20,13 +22,13 @@ export const HeroOne = styled.div`
     color: white;
     font-family: Chalkboard;
     h3 {
-      font-size: 30px;
+      font-size: 25px;
       font-weight: 700;
       line-height: 45px;
       letter-spacing: 0em;
     }
     p {
-      font-size: 20px;
+      font-size: 16px;
       font-weight: 300;
       line-height: 25px;
       letter-spacing: 0em;
@@ -34,13 +36,96 @@ export const HeroOne = styled.div`
     span {
       color: #ea7052;
     }
+    .apps {
+      display: flex;
+      // background-color: red;
+      justify-content: center;
+      width: 280px;
+      h4 {
+      }
+    }
+    .app__wrapper {
+      margin: px;
+    }
   }
   .second__content {
-    div{
-        width : 370px;
+    div {
+      width: 370px;
     }
   }
 `;
 
-export const HeroTwo = styled.div``;
-export const HeroThree = styled.div``;
+export const WrapperTwo = styled.div`
+  background-color: #fff;
+  overflow: hidden;
+  height: 380px;
+`;
+export const HeroTwo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 3rem auto;
+  width: 90%;
+  max-width: 1300px;
+  .first__content {
+    color: #000000;
+    h3 {
+      font-size: 25px;
+      font-weight: 700;
+      line-height: 45px;
+      letter-spacing: 0em;
+    }
+    p{
+      font-size: 16px;
+      font-weight: 300;
+      line-height: 25px;
+      letter-spacing: 0em;
+    }
+    span{
+        color: #ea7052;
+    }
+  }
+  .second__content{
+      div {
+          width : 300px;
+          height : 350px;
+      }
+  }
+`;
+export const WrapperThree = styled.div`
+  background-color: #f4f4f4;
+  overflow: hidden;
+  height: 380px;
+`;
+export const HeroThree = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 3rem auto;
+  width: 90%;
+  max-width: 1300px;
+  .first__content {
+   h3{
+      font-size: 25px;
+      font-weight: 700;
+      line-height: 45px;
+      letter-spacing: 0em;
+   }
+   p{
+      font-size: 16px;
+      font-weight: 300;
+      line-height: 25px;
+      letter-spacing: 0em;
+   }
+   .contact__ div{
+       display : flex;
+   }
+   .contact__ p{
+       margin : 10px;
+
+   }
+  }
+  .second__content {
+      div {
+          width : 350px;
+      }
+  }
+`;
