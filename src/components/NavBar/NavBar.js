b3491bc3e1566918c424e3e13240632435c7aac9
olaiide <horlaide909@gmail.com>
2022-04-03 06:40:37 -1200

    top

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/Logo.svg";
+import Close from "../../assets/Close.svg";
 import Button from "../Button/Button";
 import {
   ButtonContainer,
@@ -7,30 +8,62 @@ import {
   HeaderWrapper,
   LogoWrapper,
   StyledLink,
+  TopWrapper,
+  Top,
 } from "./NavBarElements";
 const NavBar = () => {
+  const [show, setShow] = useState(true);
+  const hideHandler = () => {
+    setShow(false);
+  };
   return (
-    <Container>
-      <HeaderWrapper>
-        <LogoWrapper>
-          <img src={Logo} alt='Rodufy logo' />
-        </LogoWrapper>
-        <ButtonContainer>
-          <StyledLink to={"/register"}>
-            <Button children={"Register"} width={"small"} height={"45px"} />
-          </StyledLink>
-          <StyledLink to={"/login"}>
-            <Button
-              children={"Log in"}
-              width={"small"}
-              login={true}
-              border={true}
-              height={"45px"}
-            />
-          </StyledLink>
-        </ButtonContainer>
-      </HeaderWrapper>
-    </Container>
+    <>
+      <Container>
+        
+        {show && (
+          <TopWrapper>
+            <Top>
+              <div className='first__content'>
+                <div style={{ display: "flex" }}>
+                  <p>New!</p>
+                </div>
+                <div>
+                  <span>Announcing our $15 million series A funding!</span>
+                </div>
+                <div>
+                  <img
+                    src={Close}
+                    alt='close'
+                    width='30px'
+                    className='second__content'
+                    onClick={hideHandler}
+                  />
+                </div>
+              </div>
+            </Top>
+          </TopWrapper>
+        )}
+        <HeaderWrapper>
+          <LogoWrapper>
+            <img src={Logo} alt='Rodufy logo' />
+          </LogoWrapper>
+          <ButtonContainer>
+            <StyledLink to={"/register"}>
+              <Button children={"Register"} width={"small"} height={"45px"} />
+            </StyledLink>
+            <StyledLink to={"/login"}>
+              <Button
+                children={"Log in"}
+                width={"small"}
+                login={true}
+                border={true}
+                height={"45px"}
+              />
+            </StyledLink>
+          </ButtonContainer>
+        </HeaderWrapper>
+      </Container>
+    </>
   );
 };
 
